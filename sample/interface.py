089c043f935e6550aa8365a57a4f639958a6f815
Michael Smith <mas9311@gmail.com>
2019-06-26 05:06:56 -0600

    Added new Frames (and widgets) Symbol, then AssetType, then Quantity, then CurrentAverage, then PotentialAverage. THe only way to break from this progression is to load from a file previously created. The functionality is still not tethered to the application; merely cosmetic for now.

--- sample/interface.py
+++ sample/interface.py
@@ -7,7 +7,6 @@ def run():
     # if parameter_passed != 'cli':
         root = Tk()
         GUI(root)
-        root.resizable(False, False)
         root.mainloop()
     # else:
     #     stock_average.run()
@@ -17,14 +16,24 @@ class GUI(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
         self.master.title("Compute New Average")
-        self.pack(fill=BOTH, expand=True)
+        self.pack(expand=True, fill=BOTH)
         self.root = parent
         self.root.configure(background='black', bd=0, width=500)
+        # self.root.resizable(False, False)
 
         self.symbol = StringVar()
+        self.asset_type = StringVar()
+        self.quantity = DoubleVar()
+        self.current_average = DoubleVar()
+        self.potential_average = DoubleVar()
 
         self.close_button = None
-        self.main_frame = None
+        self.symbol_frame = None
+        self.asset_type_frame = None
+        self.quantity_frame = None
+        self.current_average_frame = None
+        self.potential_average_frame = None
+
         self.create_widgets()
 
     def _close(self):
@@ -34,39 +43,70 @@ class GUI(Frame):
         self.close_button = Button(self, highlightthickness=0, text='×', command=self._close,
                                    activebackground='#444444', activeforeground='#cccccc')
         self.close_button.pack(side=TOP, anchor=NE)
-        self.main_frame = Main(self)
+        self.symbol_frame = Symbol(self)
+
+    def create_asset_type_frame(self):
+        if self.asset_type_frame is None:
+            self.asset_type_frame = AssetType(self)
+        else:
+            self.asset_type_frame.deselect_buttons()
+
+    def reload_quantity_description(self):
+        self.quantity_frame.reload_description()
+
+    def create_quantity_frame(self):
+        if self.quantity_frame is None:
+            self.quantity_frame = Quantity(self)
+
+    def create_current_average_frame(self):
+        if self.current_average_frame is None:
+            self.current_average_frame = CurrentAverage(self)
+
+    def create_potential_average_frame(self):
+        if self.potential_average_frame is None:
+            self.potential_average_frame = PotentialAverage(self)
+
+    def populate_from_file(self):
+        # Read from file, then populate the frames with the data
+        self.create_asset_type_frame()
+        self.create_quantity_frame()
+        self.create_current_average_frame()
+        self.create_potential_average_frame()
+
+    def destroy_all_frames_after_symbol(self):
+        if self.asset_type_frame:
+            self.asset_type_frame.destroy()
+            self.asset_type_frame = None
+        if self.quantity_frame:
+            self.quantity_frame.destroy()
+            self.quantity_frame = None
+        if self.current_average_frame:
+            self.current_average_frame.destroy()
+            self.current_average_frame = None
+        if self.potential_average_frame:
+            self.potential_average_frame.destroy()
+            self.potential_average_frame = None
 
 
-class Main(Frame):
+class Symbol(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
-        self.pack(fill=BOTH, expand=True)
+        self.pack(expand=True, fill=BOTH)
         self.parent = parent
+        self.root = self.parent
 
-        self.symbol_frame = None
         self.symbol_label = None
         self.symbol_entry = None
         self.symbol_description = 'Enter the ticker symbol'
         self.symbol_focused = False
-        self.create_symbol_frame()
+        self.create_symbol_widgets()
         self.update_idletasks()
 
-        self.current_average_frame = None
-        self.current_average_label = None
-        self.current_average_entry = None
-
-        self.projected_average_frame = None
-        self.projected_average_label = None
-        self.projected_average_entry = None
-
-    def create_symbol_frame(self):
-        self.symbol_frame = Frame(self)
-        self.symbol_frame.pack(fill=X, expand=True)
-
-        self.symbol_label = Label(self.symbol_frame, text="Symbol", width=15)
+    def create_symbol_widgets(self):
+        self.symbol_label = Label(self, text="Symbol", width=15)
         self.symbol_label.pack(side=LEFT, anchor=W)
 
-        self.symbol_entry = Entry(self.symbol_frame, textvariable=self.parent.symbol)
+        self.symbol_entry = Entry(self, textvariable=self.parent.symbol)
         self.symbol_entry.insert(0, self.symbol_description)
         self.symbol_entry.bind('<Enter>', self.symbol_entry_entered)
         self.symbol_entry.bind('<Leave>', self.symbol_entry_left)
@@ -79,26 +119,6 @@ class Main(Frame):
         self.symbol_entry.bind('<Control-y>', lambda e: 'break')  # disable uncommon undo (paste in tkinter)
         self.symbol_entry.bind('<Button-3>', lambda e: 'break')  # disable right-click
         self.symbol_entry.pack(side=LEFT, expand=True, fill=X)
-
-    def create_current_average_frame(self):
-        self.current_average_frame = Frame(self)
-        self.current_average_frame.pack(fill=X)
-
-        self.current_average_label = Label(self.current_average_frame, text="Current Average", width=15)
-        self.current_average_label.pack(side=LEFT, anchor=W)
-
-        self.current_average_entry = Entry(self.current_average_frame)
-        self.current_average_entry.pack(side=LEFT)
-
-    def create_projected_average_frame(self):
-        self.projected_average_frame = Frame(self)
-        self.projected_average_frame.pack(fill=X, expand=True)
-
-        self.projected_average_label = Label(self.projected_average_frame, text="Projected Average", width=15)
-        self.projected_average_label.pack(side=LEFT, expand=True)
-
-        self.projected_average_entry = Entry(self.projected_average_frame, state='readonly')
-        self.projected_average_entry.pack(side=LEFT)
 
     def symbol_entry_entered(self, _):
         if self.symbol_entry.get() == self.symbol_description:
@@ -136,23 +156,16 @@ class Main(Frame):
                 if len(self.symbol_entry.get()) > 4:
                     # If the length of the symbol is 5+, delete the last letter
                     self.symbol_entry.delete(self.symbol_entry.index(END) - 1)
-                print('Symbol:', curr_symbol, self.parent.symbol.get())
+                print('Symbol:', curr_symbol)
 
             if file_helper.file_exists(curr_symbol.lower()):
                 # If user previously saved a file for this symbol, load it!
-                self.populate_from_file()
-                return
-
-            if not self.current_average_frame:
-                self.create_current_average_frame()
+                self.parent.populate_from_file()
+            else:
+                self.parent.create_asset_type_frame()
         else:
-            # No characters remain
-            if self.current_average_frame:
-                self.current_average_frame.forget()
-                self.current_average_frame = None
-            if self.projected_average_frame:
-                self.projected_average_frame.forget()
-                self.projected_average_frame = None
+            # No characters remain in the Entry textbox, so destroy all children
+            self.parent.destroy_all_frames_after_symbol()
 
     def delete_symbol_spaces(self):
         space_position = self.symbol_entry.get().find(' ')
@@ -164,8 +177,139 @@ class Main(Frame):
             elif space_position is last_char_index:
                 self.symbol_entry.delete(last_char_index)
 
-    def populate_from_file(self):
-        if not self.current_average_frame:
-            self.create_current_average_frame()
-        if not self.projected_average_frame:
-            self.create_projected_average_frame()
+
+class AssetType(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.pack(expand=True, fill=BOTH)
+        self.parent = parent
+
+        self.asset_type_label = None
+        self.stock_button = None
+        self.crypto_button = None
+
+        self.create_asset_type_widgets()
+
+    def create_asset_type_widgets(self):
+        self.asset_type_label = Label(self, text="Asset Type", width=15)
+        self.asset_type_label.grid(row=0, column=0)
+
+        self.stock_button = Radiobutton(self, text='stock', variable=self.parent.asset_type,
+                                        val='stock', command=self.select)
+        self.stock_button.grid(row=0, column=1)
+
+        self.crypto_button = Radiobutton(self, text='crypto', variable=self.parent.asset_type,
+                                         val='cryptocurrency', command=self.select)
+        self.crypto_button.grid(row=0, column=2)
+
+    def select(self):
+        self.parent.create_quantity_frame()
+
+    def deselect_buttons(self):
+        self.stock_button.deselect()
+        self.crypto_button.deselect()
+        self.parent.reload_quantity_description()
+
+
+class Quantity(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.pack(expand=True, fill=BOTH)
+        self.parent = parent
+
+        self.quantity_label = None
+        self.quantity_entry = None
+        self.quantity_description_basic = 'Enter the # of '
+        self.quantity_description = None
+        self.quantity_focused = False
+        self.reload_description()
+
+        self.create_quantity_widgets()
+
+    def create_quantity_widgets(self):
+        self.quantity_label = Label(self, text="Quantity", width=15)
+        self.quantity_label.grid(row=0, column=0)
+
+        self.quantity_entry = Entry(self, textvariable=self.parent.quantity)
+        self.quantity_entry.insert(0, self.quantity_description)
+        self.quantity_entry.bind('<Enter>', self.quantity_entry_entered)
+        self.quantity_entry.bind('<Leave>', self.quantity_entry_left)
+        self.quantity_entry.bind('<FocusIn>', self.quantity_entry_focused)
+        self.quantity_entry.bind('<FocusOut>', self.quantity_entry_focused)
+        self.quantity_entry.bind('<KeyRelease>', self.quantity_entry_key_released)
+        self.quantity_entry.grid(row=0, column=1)
+
+    def quantity_entry_entered(self, _):
+        if self.quantity_entry.get().find(self.quantity_description_basic) is not -1:
+            self.quantity_entry.delete(0, END)
+
+    def quantity_entry_left(self, _):
+        if not self.quantity_entry.get() and not self.quantity_focused:
+            self.quantity_entry.insert(0, self.quantity_description)
+
+    def quantity_entry_key_released(self, event):
+        print(event.char)
+        if self.quantity_entry.get():
+            self.parent.create_current_average_frame()
+
+    def quantity_entry_focused(self, _):
+        self.quantity_focused = not self.quantity_focused
+        if not self.quantity_focused and not self.quantity_entry.get():
+            self.quantity_entry.insert(0, self.quantity_description)
+
+    def reload_description(self):
+        self.quantity_description = self.quantity_description_basic
+        self.quantity_description += 'stocks' if self.parent.asset_type.get().lower()[0:1] == 's' else 'coins'
+        if self.quantity_entry:
+            if self.quantity_entry.get().find(self.quantity_description_basic) != -1:
+                self.quantity_entry.delete(0, END)
+                self.quantity_entry.insert(0, self.quantity_description)
+
+
+class CurrentAverage(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.pack(expand=True, fill=BOTH)
+        self.parent = parent
+
+        self.current_average_label = None
+        self.current_average_entry = None
+        self.current_average_description = 'Enter your current Average'
+
+        self.create_current_average_widgets()
+
+    def create_current_average_widgets(self):
+        self.current_average_label = Label(self, text="Current Average", width=15)
+        self.current_average_label.grid(row=0, column=0)
+
+        self.current_average_entry = Entry(self, textvariable=self.parent.current_average)
+        self.current_average_entry.insert(0, self.current_average_description)
+        self.current_average_entry.bind('<KeyRelease>', self.current_average_entry_key_released)
+        self.current_average_entry.grid(row=0, column=1)
+
+    def current_average_entry_key_released(self, event):
+        print(event.char)
+        if self.current_average_entry.get():
+            self.parent.create_potential_average_frame()
+
+
+class PotentialAverage(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.pack(expand=True, fill=BOTH)
+        self.parent = parent
+
+        self.potential_average_label = None
+        self.potential_average_entry = None
+
+        self.create_potential_average_widgets()
+
+    def create_potential_average_widgets(self):
+        self.potential_average_label = Label(self, text="Potential Average", width=15)
+        self.potential_average_label.pack(side=LEFT, expand=True)
+
+        self.potential_average_entry = Entry(self, state='readonly')
+        self.potential_average_entry.pack(side=LEFT)
+
+
+
