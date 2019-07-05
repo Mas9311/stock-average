236cf0fd418edbd5a95da31bf2c541832e2a195e
Michael Smith <mas9311@gmail.com>
2019-07-04 21:06:46 -0600

    Update menu's methods to conform with the new way of handling variables.  - The way it creates the output dialog does not look any different visually.   - The way the 'menu' is created is now more flexible to it's inputs and choices of outputs.

--- sample/menu.py
+++ sample/menu.py
@@ -1,71 +1,106 @@
 from sample import format, modify_stock
 
 
-def y_or_n(description, first, second):
+def create_menu_output(description, options):
+    menu_output = f'{description}\n [Enter] {options[0]}.\n'
+    for index in range(1, len(options)):
+        menu_output += f' [{index}] {options[index]}.\n'
+    return menu_output
+
+
+def ask_options(description, options):
     """Prints how to receive input from the user."""
-    return input(f'{description}\n'
-                 f'\tEnter any key to {first}\n'
-                 f'  or\n'
-                 f'\tPress [enter] to {second}.\n').strip().lower()
+    # if isinstance(options, str):
+    #     options = [options]
+    last_index = len(options) - 1
+    menu_output = create_menu_output(description, options)
+    while True:
+        output = input(f'{menu_output}> ').strip()
+        if not output:
+            # user selected the first option: nothing [Enter]ed
+            return None
+        if len(options) is 2:
+            # only two options: return the numerical index regardless of what the user typed
+            return last_index
+        try:
+            output = int(output)
+            if 1 <= output <= last_index:
+                return output
+            else:
+                print('Invalid:', output, f'is not in valid range [1, {last_index}].\n')
+        except ValueError:
+            print('Invalid:', repr(output), 'is not a valid option.\n')
 
 
-def modify_menu(my_stock):
+def modify_menu(cli):
     """Prints the initial menu to determine if the user wants to modify any of the values printed."""
-    return y_or_n(f'{my_stock}\n'
-                  f'Would you like to update any of the values listed for {my_stock.get_symbol()}?',
-                  f'modify a value',
-                  f'skip this step')
+    print(cli)
+    output = ask_options(f'Would you like to update any of the values listed for {cli.get_symbol()}?',
+                       ['to skip this step', 'to modify a value'])
+    print()
+    return output
 
 
-def update_menu(my_stock):
+def update_menu(cli):
     """The user has selected to modify a value. Which value to modify?"""
-    return input(f'\nSelect the option that you wish to modify from the list below:\n'
-                 f'\t1: Cryptocurrency:     {"Yes" if my_stock.crypto else "No"}\n'
-                 f'\t2: Purchased average:  {format.price(my_stock.purchased_average, my_stock.precision)}\n'
-                 f'\t3: Purchased quantity: {my_stock.purchased_quantity}\n'
-                 f'\t4: Current price:      {format.price(my_stock.current_price, my_stock.precision)}\n').strip()
+    mod = 'to modify the '
+    output = ask_options(
+        'Select the option that you wish to modify from the list below:',
+        [
+            'to return',
+            f'{mod}Asset type:         {cli.asset_type.input}',
+            f'{mod}Quantity:           {cli.quantity.input}',
+            f'{mod}Current average:    ${cli.current_average.input}',
+            f'{mod}Current price:      ${cli.current_price.input}'
+        ]
+    )
+    return output
 
 
-def update(my_stock):
+def update(cli):
     """Macro-function that determines which other functions to call based on the user input provided."""
     while True:
         while True:
-            if not modify_menu(my_stock):
+            if not modify_menu(cli):
+                # user does not want to modify any of the CLI variables
                 return
             try:
-                selection = int(update_menu(my_stock)[0])
+                selection = update_menu(cli)
+                selection = int(selection)
                 if 1 <= selection <= 4:
                     # The only way to continue is with a valid number between 1 and 4
+                    print()
                     break
                 print(format.Feedback(True, f'\'{selection}\' must be an option between 1 and 4.'))
             except ValueError:
                 print(format.Feedback(True, f'The selection is not a valid number.'))
             except IndexError:
                 print(format.Feedback(True, f'The selection cannot be left blank.'))
-        modify_stock.switcher(convert_selection_update(selection), my_stock)
+
+        # user has input a number between [1, 4]
+        modify_stock.switcher(convert_selection_update(selection), cli)
+        # continue: ask user again if they want to modify any variables
 
 
-def convert_selection_update(answer):
+def convert_selection_update(selection):
     """Converts the integer to a string as to not confuse those that review the code."""
-    if answer is 1:
-        return f'crypto'
-    elif answer is 2:
-        return f'purchased average'
-    elif answer is 3:
-        return f'purchased quantity'
-    elif answer is 4:
+    if selection is 1:
+        return f'asset type'
+    elif selection is 2:
+        return f'quantity'
+    elif selection is 3:
+        return f'current average'
+    elif selection is 4:
         return f'current price'
 
 
 def ending_menu(symbol):
     """Prints the final menu to determine what the user wants to do now."""
-    if not y_or_n(f'Do you wish to continue or exit?',
-                  f'continue finding averages',
-                  f'exit the program'):
+    if ask_options(f'Do you wish to continue or exit?',
+                   ['to exit the program', 'to continue finding averages']) is None:
         return f'quit'
-    if not y_or_n(f'Do you want to choose another symbol or continue with {symbol}?',
-                  f'choose a different symbol',
-                  f'continue with {symbol}'):
+    if not ask_options(f'Do you want to choose another symbol or continue with {symbol}?',
+                       [f'continue with {symbol}', 'choose a different symbol']):
         return f'same'
     print(f'\n\n\n\n\n\n\n\n\n\n\n\n')
     return f'different'
