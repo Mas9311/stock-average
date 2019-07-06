0dade27751cdcccc77cb92f8e2dcbf658980f1a3
Michael Smith <mas9311@gmail.com>
2019-07-05 18:11:26 -0600

    Converts user_input from str() to str(int || float) Conforms to the behavior of uniform once printing: '\n> ' to message

--- sample/cli/Numeric.py
+++ sample/cli/Numeric.py
@@ -10,15 +10,43 @@ class Numeric(TextBaseClass):
         if self.user_input.count(','):
             # If the user input a comma (1,000,000), delete the commas.
             self.user_input = self.user_input.replace(',', '')
+
         if self.user_input.count('.') > 1:
-            print('Only one decimal point allowed.')
+            print('Invalid: Only one decimal point allowed.', end='')
             return False
-        return len(self.user_input) > 0
+        elif len(self.user_input) is 0:
+            print('Invalid: input cannot be empty.', end='')
+            return False
+        return True
+
+    def input_is_valid(self):
+        if not super().input_is_valid():
+            # one or more characters entered is/are invalid.
+            print()
+            return False
+        # every character entered is valid.
+        self.convert_to_number()
+        return True
+
+    def convert_to_number(self):
+        # convert string to a number {3.0 or 3.14}
+        self.user_input = float(self.user_input)
+
+        if self.user_input == int(self.user_input):
+            # 3.0 => 3
+            self.user_input = int(self.user_input)
+
+        # convert the number back to a string
+        self.user_input = str(self.user_input)
 
 
 class Currency(Numeric):
     def __init__(self, parent, message):
         super().__init__(parent, message)
         self.intro_char = '$'
-        self.valid_chars = [('0', '9'), '.']
 
+    def convert_to_number(self):
+        """Modifies the user_input to """
+        self.user_input = str(float(self.user_input))
+        for i in range(len(self.user_input) - 1 - self.user_input.find('.')):
+            self.user_input += '0'
