968d0007dc8914e61edc7b8a8a9bb39b7c2bb423
Michael Smith <mas9311@gmail.com>
2019-07-07 01:33:12 -0600

    Refactored how to write data to file get_file now converts to symbol to lower(), as opposed to sending it in lowercase. modify_file no longer: reads data from the file, applies 1-line update, writes to file.  - now it: reads variables and write to the file.

--- sample/file_helper.py
+++ sample/file_helper.py
@@ -1,6 +1,6 @@
-from sample import format, menu, modify_stock
+from sample import format, menu
 import os
-import sys
+# import sys
 
 
 def get_folder():
@@ -9,30 +9,33 @@ def get_folder():
     return os.path.join(os.getcwd(), 'symbols')
 
 
-def get_file(stock_symbol):
-    """Returns the given stock's file path."""
-    return os.path.join(get_folder(), stock_symbol)
+def get_file(symbol):
+    """Returns the given symbol's file path."""
+    return os.path.join(get_folder(), symbol.lower())
 
 
-def modify_file(my_stock, selection):
+def modify_file(cli):
     """This function is called once the user has selected to modify a given value of the stock."""
-    lines = retrieve_file_data(my_stock.get_symbol().lower())
-    if selection == f'crypto':
-        lines[0] = f'{my_stock.get_symbol()} is a cryptocurrency!' if my_stock.crypto else f''
-    elif selection == f'purchased average':
-        lines[1] = my_stock.purchased_average
-    elif selection == f'purchased quantity':
-        lines[2] = my_stock.purchased_quantity
-    elif selection == f'current price':
-        lines[3] = my_stock.current_price
+    print(cli.symbol, cli.get_symbol())
+    write_data_to_file(
+        cli.get_symbol(),
+        [
+            f'{cli.get_symbol()} is a {cli.asset_type}',
+            f'{cli.get_quantity()}',
+            f'{cli.get_current_average()}',
+            f'{cli.get_current_price()}'
+        ]
+    )
+    print(format.Feedback(False, f'{cli.get_symbol()} has been updated.'))
 
-    with open(get_file(my_stock.symbol.lower()), 'w') as f:
-        f.write(f'{lines[0]}\n')
-        f.write(f'{repr(lines[1])}\n')
-        f.write(f'{repr(lines[2])}\n')
-        f.write(f'{repr(lines[3])}\n')
-        f.close()
-    print(format.Feedback(False, f'{my_stock.get_symbol()} has been updated.'))
+
+def write_data_to_file(symbol, data):
+    with open(get_file(symbol), 'w') as symbol_file:
+        symbol_file.write(f'{data[0]}\n')
+        symbol_file.write(f'{data[1]}\n')
+        symbol_file.write(f'{data[2]}\n')
+        symbol_file.write(f'{data[3]}\n')
+        symbol_file.close()
 
 
 def make_sure_dir_exists():
@@ -77,55 +80,48 @@ def file_exists(symbol):
 
 
 def assign_file_data_to_variables(cli):
-    (asset_type, current_average, quantity, current_price) = retrieve_file_data(cli.get_symbol())
-    if asset_type.lower().count(cli.asset_type.valid_options[0]):
+    (asset_type, quantity, current_average, current_price) = retrieve_file_data(cli.get_symbol())
+    if asset_type.lower().count('stock'):
         cli.asset_type.input = cli.asset_type.valid_options[0]
     else:
         cli.asset_type.input = cli.asset_type.valid_options[1]
-    cli.current_average.input = str(current_average)
-    cli.quantity.input = str(quantity)
-    cli.current_price.input = str(current_price)
+    cli.quantity.input = quantity
+    cli.current_average.input = current_average
+    cli.current_price.input = current_price
 
 
-def make_file(stock_symbol):
+def create_new_file(cli):
     """The stock file does not currently exist in the folder.
     We will now make one so you can use it again later."""
-    print(f'The {stock_symbol} file is not in the symbols folder, so we will make it now.\n\n')
-    file_path = get_file(stock_symbol)
-    stock_symbol = stock_symbol.upper()
-    with open(file_path, 'w') as new_file:
-        crypto_answer = menu.y_or_n(f'Is {stock_symbol} a cryptocurrency?',
-                                    f'save it is a cryptocurrency',
-                                    f'save it as a regular stock')
-        if crypto_answer:
-            new_file.write(f'{stock_symbol} is a cryptocurrency!')
-        new_file.write('\n')
-        type_of = f'share' if not crypto_answer else f'coin'
-        shares = input(f'How many {type_of}s of {stock_symbol} have you purchased?\n').strip()
-        new_file.write(input(f'What is your current average of {stock_symbol}?\n').strip().lower() + '\n')
-        new_file.write(f'{shares}\n')
-        new_file.write(input(f'What is the current price of {stock_symbol}?\n').strip().lower() + '\n')
-        new_file.close()
+    write_data_to_file(
+        cli.get_symbol(),
+        [
+            f'{cli.get_symbol()} is a {cli.get_asset_type()}',
+            cli.get_quantity(),
+            cli.get_current_average(),
+            cli.get_current_price()
+        ]
+    )
 
 
-def get_symbol():
-    if len(sys.argv) >= 2:
-        symbol = sys.argv[1].strip().lower()
-        try:
-            float(symbol)  # This should fail, meaning correct arguments given
-            print(format.Feedback(True, f'First arg should be the symbol, not the current price.'))
-        except ValueError:
-            return symbol
-    else:
-        return input(f'What is the symbol of the stock or cryptocurrency?\n').strip().lower()
-
-
-def get_current_price(my_stock):
-    if len(sys.argv) >= 3:
-        value = sys.argv[2]  # current price
-        modify_stock.validate_input(my_stock, f'current_price', value)
-        my_stock.update_quantity_format()
-        modify_file(my_stock, f'current price')
+# def get_symbol():
+#     if len(sys.argv) >= 2:
+#         symbol = sys.argv[1].strip().lower()
+#         try:
+#             float(symbol)  # This should fail, meaning correct arguments given
+#             print(format.Feedback(True, f'First arg should be the symbol, not the current price.'))
+#         except ValueError:
+#             return symbol
+#     else:
+#         return input(f'What is the symbol of the stock or cryptocurrency?\n').strip().lower()
+#
+#
+# def get_current_price(my_stock):
+#     if len(sys.argv) >= 3:
+#         value = sys.argv[2]  # current price
+#         modify_stock.validate_input(my_stock, f'current_price', value)
+#         my_stock.update_quantity_format()
+#         modify_file(my_stock, f'current price')
 
 
 def retrieve_file_data(symbol):
@@ -138,7 +134,7 @@ def retrieve_file_data(symbol):
         lines = stock_file.read().splitlines()
         stock_file.close()
 
-        return [lines[0].strip(),               # is this a crypto?
-                abs(float(lines[1].strip())),   # purchased average
-                abs(float(lines[2].strip())),   # purchased quantity
+        return [lines[0].strip(),               # asset type: stock or cryptocurrency
+                abs(float(lines[1].strip())),   # quantity
+                abs(float(lines[2].strip())),   # current average
                 abs(float(lines[3].strip()))]   # current price
