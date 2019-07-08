7b1455a2f9da57758e6862144ee4b78ce65f31bd
Michael Smith <mas9311@gmail.com>
2019-07-08 00:00:14 -0600

    Simplify average.PotentialAverage's behavior. This class is used to predict the user's potential average(s) The culmination of gathering all previous input leads to the crux of the project: finding your new average(s). See the README.md's formula for the details of the math behind the numerator/denominator. Simplified __str__ method which returns the message of what the numbers mean.

--- sample/average.py
+++ sample/average.py
@@ -1,59 +1,38 @@
-from . import format
+from sample.format import Price
 
 
-class Average:
-    """This class keeps track of the running totals of the numerator and denominator for a faster calculation."""
-    def __init__(self, my_stock):
-        self.crypto = bool(my_stock.crypto)
-        self.precision = my_stock.precision
-        self.numerator = my_stock.purchased_average * my_stock.purchased_quantity
-        self.denominator = my_stock.purchased_quantity
-        self.additional_numerator = 0.00
-        self.additional_denominator = 0.00
+class PotentialAverage:
+    def __init__(self, parent, curr_iter, cost_per):
+        self.parent = parent
 
-    def update_crypto(self, my_stock):
-        """Updates the crypto variable and the dependent variable."""
-        self.crypto = my_stock.crypto
-        self.precision = my_stock.precision
+        self.curr_iter = curr_iter
+        self.cost_per = cost_per
+        self.additional_cost = self.get_additional_cost()
+        if self.parent.get_asset_type() != 'stock':
+            # convert curr_iter to a fraction relative to its coin
+            self.curr_iter = self.cost_per * self.curr_iter / self.parent.get_current_price()
 
-    def add_new(self, add_to_numerator, add_to_denominator):
-        """Adds a new instance to the numerator and increments the denominator accordingly."""
-        self.additional_numerator += add_to_numerator
-        self.additional_denominator += add_to_denominator
+        self.numerator = self.parent.get_quantity() * self.parent.get_current_average()  # (n * oldPrice)
+        self.additional_numerator = self.curr_iter * self.parent.get_current_price()  # (x *currentPrice)
+        self.denominator = self.parent.get_quantity()  # n
+        self.additional_denominator = self.curr_iter
 
-    def retrieve_average(self):
+    def get_additional_cost(self):
+        return round(self.curr_iter * self.cost_per, 2)
+
+    def average(self):
         """Returns the average is float format without rounding."""
-        return (self.numerator + self.additional_numerator) / (self.denominator + self.additional_denominator)
+        num = self.numerator + self.additional_numerator
+        den = self.denominator + self.additional_denominator
+        return round(num / den, 3)
 
-    def avg(self):
-        """Returns the potential average in rounded float format."""
-        return round(self.retrieve_average(), self.precision)
+    def get_coordinates(self):
+        """Returns the (x, y) coordinates to plot this PotentialAverage instance on a graph"""
+        return self.get_additional_cost(), self.average()
 
     def __str__(self):
         """Returns the potential average in string format."""
-        return format.price(self.retrieve_average(), self.precision)
-
-
-class PotentialOutcome:
-    def __init__(self, my_allotted, potential_index, potential_cost, potential_average):
-        self.symbol = my_allotted.symbol
-        self.crypto = my_allotted.crypto
-        self.precision = my_allotted.precision
-        self.type_of = my_allotted.type_of
-
-        self.index = int(potential_index)
-        self.cost = potential_cost
-        self.average = float(potential_average)
-
-    def update_crypto(self, my_allotted):
-        """Updates the crypto variable and everything else that depends on it."""
-        self.crypto = my_allotted.crypto
-        self.precision = my_allotted.precision
-        self.type_of = my_allotted.type_of
-
-    def __str__(self):
-        if not self.crypto:
-            type_str = f'buying {format.multiple(self.index, "additional ", self.type_of)}'
-        else:
-            type_str = f'spending {format.price(self.cost, 2)}'
-        return f'After {type_str}, \tyour new average would be\t{format.price(self.average, self.precision)}.'
+        return {
+            f'After spending {Price(self.additional_cost, 2)}, '
+            f'your new average would be {Price(self.average(), 2)}.'
+        }
