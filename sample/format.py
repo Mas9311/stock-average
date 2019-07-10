f0b193359b161b61be54b90cf6b0edc7cf258274
Michael Smith <mas9311@gmail.com>
2019-07-10 06:36:27 -0600

    Price now prints with commas and without scientific notation. Price now asseses when to keep adding precision (recursively) until at least one non-zero value is displayed. This Price will be copy/pasted into most of my projects, so I took the time to perfect it. Sadly, round(0.5, 0) returns 0, but round(1.5, 0) returns 2.  - This is not a bug. Check out Python 3 documentation for more details (on how floating point operates)

--- sample/format.py
+++ sample/format.py
@@ -58,12 +58,15 @@ class Feedback:
 class Price:
     """Converts the money input into a price format with precision.
     Examples include:
+    Price(0.000000016)  returns $0.00000002
     Price(0.499, 0)     returns $0
     Price(0.500, 0)     returns $1
     Price(11.09, 1)     returns $11.1
     Price(22, 2)        returns $22.00
     Price(44.1, 4)      returns $44.1000
-    Price(88.1234, 8)   returns $88.12340000"""
+    Price(88.1234, 8)   returns $88.12340000
+    Price(123456)       returns $123,456.00
+    Price(1234567)      returns $1,234,567.00"""
     def __init__(self, money, precision=2):
         self.money = money
         self.precision = precision
@@ -73,20 +76,45 @@ class Price:
 
     def create_output(self):
         try:
-            self.money = abs(float(self.money))
+            self.money = abs(float(str(self.money)))
             self.precision = abs(int(self.precision))
         except ValueError:
             print('Invalid: the money parameter in Price is not a number')
+            self.output = 'Error'
             return
 
         if self.precision == 0:
             self.output += str(int(round(float(self.money), self.precision)))
+            self.place_commas()
             return
 
-        # truncate any decimals if the money is longer than the precision
-        self.money = str(round(float(self.money), self.precision))
-        zeros = '0' * (self.precision - (len(self.money) - 1 - self.money.find('.')))
-        self.output += self.money + zeros
+        if self.money and not round(float(self.money), self.precision):
+            # if money != 0.00, but rounding using this precision would lead to a 0.00 dollar value:
+            # recursively call Price with precision+1 until money holds some value > 0.00
+            self.output = Price(self.money, self.precision + 1).output
+        else:
+            # truncate the least-significant decimal digits if money trails farther than precision
+            self.money = str(round(float(self.money), self.precision))
+            if self.money.find('e-'):
+                # if the money is in scientific notation:
+                # convert back to decimal (max of 14 '0's after '.')
+                self.money = ("%0.15f" % float(self.money)).rstrip('0')
+            
+            # create the leading zeros if precision is longer than 
+            # the location of the least-significant decimal digit
+            zeros = '0' * (self.precision - (len(self.money) - 1 - self.money.find('.')))
+            # combine the dollar sign, money, and trailing zeros
+            self.output += self.money + zeros
+            self.place_commas()
+
+    def place_commas(self):
+        count = 0
+        for i in range(self.output.find('.') - 1, 1, -1):
+            # walks through the characteristic digits (left-side of decimal point) backwards
+            # and places a comma in the appropriate location 1000 => 1,000
+            count += 1
+            if count % 3 == 0:
+                self.output = self.output[:i] + ',' + self.output[i:]
 
     def __str__(self):
         """Returns the money in string format by either adding zeros or truncating them."""
