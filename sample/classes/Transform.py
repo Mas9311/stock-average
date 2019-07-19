7fb98fdf19ec037d590d9604311601cf37904577
Michael Smith <mas9311@gmail.com>
2019-07-19 00:17:12 -0600

    Graph display Frame is complete with axes ticks, axes labels, and gridlines. Refactored Graph.create_entries() into smaller methods. If y_axis is < $.20 (20 cents), manually compile and place y_axis ticks. Transform overrides Radio's bind_radio_frame method with pass. After destroying Transform's children, resize the root Frame. Removed extra whitespace in Scale's output labels. Cleaned import statements.

--- sample/classes/Transform.py
+++ sample/classes/Transform.py
@@ -1,10 +1,11 @@
-from sample.classes.Radio import GuiRadio
-from sample.format import Price
-
 from tkinter import *
 import matplotlib
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+
+from sample.classes.Radio import GuiRadio
+from sample.format import Price
+
 matplotlib.use("TkAgg")
 
 
@@ -20,12 +21,14 @@ class Transform(GuiRadio):
 
         self.create_widgets()
 
+    def bind_radio_frame(self):
+        pass
+
     def create_widgets(self):
         """GuiRadio already create_widgets(), so no need to re-create.
         Selects the first button (intentionally) by default."""
         self.buttons[0].select()
-        self.create_scale()
-        self.create_display_frame()
+        self.update()
 
     def create_scale(self):
         self.destroy_scale()
@@ -47,26 +50,25 @@ class Transform(GuiRadio):
         if self.scale_frame:
             self.scale_frame.destroy()
             self.scale_frame = None
+            self.parent.resize_frame()
 
     def destroy_display_frame(self):
         if self.display_frame:
             self.display_frame.destroy()
             self.display_frame = None
+            self.parent.resize_frame()
 
     def select(self, value):
         self.string_var.set(value)
-        super().select(value)
         if not self.string_var.get():
             self.destroy_display_frame()
             self.deselect_buttons()
             self.parent.resize_frame()
-        else:
-            self.parent.calculate_potential_averages()
-            self.create_display_frame()
+        super().select(value)
 
-    def update_scale(self):
-        if self.scale_frame:
-            self.scale_frame.update()
+    def update(self):
+        self.create_scale()
+        self.create_display_frame()
 
 
 class ScaleFrame(Frame):
@@ -93,7 +95,7 @@ class ScaleFrame(Frame):
         self.update()
 
     def create_label(self):
-        self.label = Label(self, text='Increments of ')
+        self.label = Label(self, text='Increments of')
         self.label.grid(row=0, column=0)
 
     def create_amount_entry(self):
@@ -117,7 +119,7 @@ class ScaleFrame(Frame):
         key = 'amount_per'
         if key in self.gui.arg_dict.keys() and self.gui.arg_dict[key]:
             self.amount_entry.configure(state=NORMAL)
-            self.amount_per.set(self.gui.arg_dict[key])
+            self.amount_per.set(round(self.gui.arg_dict[key], 4))
             self.amount_entry.configure(state='readonly', width=len(self.amount_per.get()))
 
     def update_cost_per(self):
@@ -131,7 +133,7 @@ class ScaleFrame(Frame):
         type_of_text = 'share' if self.gui.arg_dict['asset_type'] == 'stock' else 'coin'
         if self.amount_per.get():
             type_of_text += '' if float(self.amount_per.get()) == 1.0 else 's'
-        type_of_text += ' at '
+        type_of_text += ' at'
         self.type_of_label.configure(text=type_of_text)
 
 
@@ -179,25 +181,93 @@ class Graph(Frame):
     def __init__(self, parent):
         self.gui = parent.parent
         Frame.__init__(self, master=self.gui)
-        self.pack(side=BOTTOM)
+        self.pack(side=LEFT)
+
         self.canvas = None
+        self.figure = None
+        self.subplot = None
+        self.x_axis = None
+        self.y_axis = None
+
+        self.y_max = None
+        self.y_min = None
+        self.x_max = None
+        self.x_min = None
+        self.y_offset = None
+        self.x_offset = None
 
         self.create_entries()
 
     def create_entries(self):
-        f = Figure(figsize=(4, 2))
-        a = f.add_subplot(111)
-        x_axis = []
-        y_axis = []
+        if not self.gui.arg_dict['potential_average']:
+            return
+
+        self.create_subplot()
+        self.set_axes()
+        self.set_axes_offset()
+        self.set_subplot_axes()
+
+        self.set_y_axis_ticks()
+        self.set_x_axis_ticks()
+
+        self.plot_graph()
+
+    def create_subplot(self):
+        self.figure = Figure(figsize=(4, 3), dpi=100)
+        self.subplot = self.figure.add_subplot(111)
+
+    def set_axes(self):
+        self.y_axis = []
+        self.x_axis = []
+
         for pa in self.gui.arg_dict['potential_average']:
             x, y = pa.get_coordinates()
-            x_axis.append(x)
-            y_axis.append(y)
-        a.plot(x_axis, y_axis)
+            self.x_axis.append(round(x, 2))
+            self.y_axis.append(round(y, 2))
 
-        self.canvas = FigureCanvasTkAgg(f, self)
+        self.y_min = min(self.y_axis)
+        self.y_max = max(self.y_axis)
+        self.x_min = min(self.x_axis)
+        self.x_max = max(self.x_axis)
+
+    def set_axes_offset(self):
+        self.y_offset = abs(self.y_axis[1] - self.y_axis[0]) / 10
+        self.x_offset = abs(self.x_axis[1] - self.x_axis[0]) / 5
+
+    def set_subplot_axes(self):
+        self.subplot.set_ylabel('Potential Average')
+        self.subplot.set_xlabel('Additional Cost')
+        self.subplot.set_ylim((self.y_min - self.y_offset, self.y_max + self.y_offset))
+        self.subplot.set_xlim((self.x_min - self.x_offset, self.x_max + self.x_offset))
+
+    def set_y_axis_ticks(self):
+        if self.y_max - self.y_min <= .20:
+            # if the scale is less than 20 cents, manually set the y_axis ticks in pennies.
+            y_difference = (self.y_max - self.y_min) // 0.01
+            y_ticks = [round(self.y_min + (0.01 * l), 2) for l in range(-1, int(y_difference + 2))]
+            self.subplot.set_yticks(y_ticks)
+            self.subplot.set_yticklabels(y_ticks, fontsize=7)
+        # else: allow matplotlib to generate and place the y_axis ticks.
+
+    def set_x_axis_ticks(self):
+        self.subplot.set_xticks(self.x_axis)
+
+        if self.x_axis[-1] < 100:
+            # if the x_axis labels will not overlap => keep labels horizontal
+            self.subplot.set_xticklabels(self.x_axis, fontsize=7)
+        else:
+            # else: the x_axis labels would overlap => rotate the labels
+            self.subplot.set_xticklabels(self.x_axis, fontsize=7, rotation=23, ha='right')
+
+    def plot_graph(self):
+        self.subplot.grid(True)
+        x_axis = [-self.x_axis[1]] + self.x_axis
+        y_axis = [self.gui.arg_dict['potential_average'][0].get_coordinates()[1]] + self.y_axis
+        self.subplot.plot(x_axis, y_axis)
+
+        self.canvas = FigureCanvasTkAgg(figure=self.figure, master=self)
         self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
-
+        self.figure.tight_layout()
+        self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)
         self.gui.resize_frame()
-
+        # TODO: cursor is extracted from its current Frame and placed within Figure...
